999ff1a141e10df0cd723578b29b650567d844c5
Thomas131104 <periodgottagotell193972@gmail.com>
2025-12-04 02:10:35 +0700

    fixing bug

--- backend/app/app.py
+++ backend/app/app.py
@@ -18,7 +18,11 @@ def create_app():
     app = FastAPI(lifespan=lifespan)
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://my-frontend.onrender.com"],
+        allow_origins=[
+            "https://my-frontend.onrender.com",        
+            "http://127.0.0.1:5500",
+            "http://localhost:5500"
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
