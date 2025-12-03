b8295141dc9f8d409b4a4625aa9eb86d6067b51f
Thomas131104 <periodgottagotell193972@gmail.com>
2025-12-03 12:07:46 +0700

    fixing route in backend

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 import asyncpg
 from .database import DATABASE_URL, init_db
@@ -13,7 +14,16 @@ async def lifespan(app: FastAPI):
 
 
 def create_app():
+    
     app = FastAPI(lifespan=lifespan)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["https://my-frontend.onrender.com"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(api)
 
     
